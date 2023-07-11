846ee21093adedeb80b04410d2c3aaf59245bc26
Corey | PC <iJabba22@gmail.com>
2023-07-11 20:50:32 +0100

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+        <section className="container grid items-center gap-4 pb-8 pt-6 md:py-10">
+
+        </section>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import electron from 'vite-plugin-electron'
+import renderer from 'vite-plugin-electron-renderer'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    electron([
+      {
+        // Main-Process entry file of the Electron App.
+        entry: 'electron/main.ts',
+      },
+      {
+        entry: 'electron/preload.ts',
+        onstart(options) {
+          // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
+          // instead of restarting the entire Electron App.
+          options.reload()
+        },
+      },
+    ]),
+    renderer(),
+  ],
+  resolve: {
+    alias: {
+        // Resolve the path to the Preload-Script.
+        // This allows to use `import { ipcRenderer } from 'electron'` in the Preload-Script.
+        electron: require.resolve('electron'),
+        "@": "/src",
+    }
+  }
+})
